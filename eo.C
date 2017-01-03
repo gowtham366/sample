c582b591e4b248ab50d9921e32f39afdc79d2039
Gowtham C <gowtham366.vpi@gmail.com>
2017-01-03 17:13:55 +0530

    Create eo.C

---
+++ eo.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int k;
+    scanf("%d",&k);
+    if(k%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+       printf("odd");
+    }
+    return 0;
+}
