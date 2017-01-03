c9d2c17edda3c582310b60392829dc394dc4e4e1
Gowtham C <gowtham366.vpi@gmail.com>
2017-01-03 17:00:56 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class GFG {
+public static void main(String[] args) 
+{
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+if(a<0)
+{
+System.out.print("Negative");
+}
+else if(a>0)
+{
+System.out.print("Positive");
+}
+else
+{
+System.out.print("Zero");
+}
+}
+}
